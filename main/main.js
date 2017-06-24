3366c75e236cb82cfe93136dbbde98a52a50956d
brobird <netassadove@gmail.com>
2017-06-24 17:39:45 +0800

    update again

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}\n
-    o) 最大值 = ${sequence.maximun()}\n
+    o) 最大值 = ${sequence.maximum()}\n
     o) 元素数量 = ${sequence.length}\n
     o) 平均值 = ${sequence.average()}\n
 // Write your code here
