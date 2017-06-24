8e7b79791c18c5145a43e512bb3f879b31ed1dfa
brobird <netassadove@gmail.com>
2017-06-24 18:01:38 +0800

    update main.js 11

--- main/main.js
+++ main/main.js
@@ -1,9 +1,6 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}\no)\
-最大值 = ${sequence.maximum()}\no)\
-元素数量 = ${sequence.length}\no)\
-平均值 = ${sequence.average()}`);
+    console.log(`o) 最小值 = ${sequence.minimum()}\no) 最大值 = ${sequence.maximum()}\no) 元素数量 = ${sequence.length}\no) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
